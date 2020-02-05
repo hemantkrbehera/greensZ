d85787b360eb348b4f37d3de5ed9ce3536bd4612
hemantkrbehera <57291174+hemantkrbehera@users.noreply.github.com>
2020-02-05 11:44:16 +0530

    something is changed

--- hr1.sql
+++ hr1.sql
@@ -1,8 +1,8 @@
  declare
- type t1 is record(a  varchar2(30),b number(20));
- v1 t1;
+ type greens is record(a  varchar2(30),b number(20));
+ v1 greens;
  begin
- select first_name,salary into v1 from employees where employee_id=100;
+ select first_name,salary,hire_Date into v1 from employees where employee_id=100;
  dbms_output.put_line(v1.a);
  dbms_output.put_line(v1.b);
  end;
